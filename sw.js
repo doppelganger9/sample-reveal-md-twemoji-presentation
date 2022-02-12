0fdc4810fdc567220406fad95757cc19d6c40ca2
David LACOURT <david.lacourt+github@gmail.com>
2022-02-12 18:19:15 +0100

    broken sw

--- sw.js
+++ sw.js
@@ -1,37 +1,59 @@
 /**
  * Basic Service Worker implementation.
  * Caches all static resources found
+ * from https://googlechrome.github.io/samples/service-worker/basic/
  */
-// When the SW is installed, it will populate the Browser's cache with some files.
-// To check this, use the Developer Console / Application / cache.
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
+
+const PRECACHE_URLS = [
+  __REPLACE_FILES_HERE__
+];
+
+// The install handler takes care of precaching the resources we always need.
 this.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('v1').then(function(cache) {
-      return cache.addAll([
-__REPLACE_FILES_HERE__
-      ]);
-    })
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())    
   );
 });
 
-// SW intercepts "fetch" events, and as a Proxy can intercept a network request.
-// thus we can use the cache to return cached stuff instead of making the request.
-this.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .catch(function() {
-        // if CACHE MISSES, then let the request pass.
-        return fetch(event.request).
-          then(function(response) {
-            // And cache this new response
-            return caches.open('v1').then(function(cache) {
-              cache.put(event.request, response.clone());
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', event => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
+  );
+});
+
+// The fetch handler serves responses for same-origin resources from a cache.
+// If no response is found, it populates the runtime cache with the response
+// from the network before returning it to the page.
+self.addEventListener('fetch', event => {
+  // Skip cross-origin requests, like those for Google Analytics.
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
+            // Put a copy of the response in the runtime cache.
+            return cache.put(event.request, response.clone()).then(() => {
               return response;
             });
-          }).catch(function() {
-            // if fetch is in error, we can here provide a default resource file.
-            // TODO
           });
+        });
       })
-  );
+    );
+  }
 });
